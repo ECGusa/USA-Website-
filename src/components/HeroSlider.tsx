56dbc421b74f57fe474530a1cf05a2af55820abe
ECGusa <webecgusa@gmail.com>
2026-03-14 21:40:35 -0400

    Updated HeroSlider.tsx

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -51,7 +51,7 @@ const HeroSlider = () => {
     {
       id: 3,
       type: 'image',
-      background: '/Slider5v2.png',
+      background: '/Slider5v3.jpg',
       title: 'Annual USA Leaders Retreat',
       subtitle: 'May 1-3',
       description: 'Setting My Kairos Year to Plant, Build & Multiply',
